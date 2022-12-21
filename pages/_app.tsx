be92ae5e736ad29ece5afa3d71df08bc6f0a2201
Stephen Gbolagade <61961222+stephengade@users.noreply.github.com>
2022-12-21 11:34:55 +0100

    Troubleshooting Vercel deployment error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,58 +5,12 @@ import "../styles/index.css";
 import InjectTailwind from '../components/Modules/InjectTailwind';
 
 
-// web 3 configuration
-
-import {
-  EthereumClient,
-  modalConnectors,
-  walletConnectProvider,
-} from "@web3modal/ethereum";
-
-
-import { Web3Modal } from "@web3modal/react";
-
-import { configureChains, createClient, WagmiConfig } from "wagmi";
-
-import { arbitrum, mainnet, polygon, optimism, avalanche, bsc, sepolia, localhost} from "wagmi/chains";
-
-export const ProjectId = process?.env?.PROJECTID;
-
-
-const chains = [arbitrum, mainnet, polygon, optimism, avalanche, bsc, sepolia, localhost];
-
-// Wagmi client
-const { provider } = configureChains(chains, [
-walletConnectProvider({ projectId: ProjectId as any }),
-]);
-const wagmiClient = createClient({
-autoConnect: true,
-connectors: modalConnectors({ appName: "Exx Network Test", chains }),
-provider,
-});
-
-// Web3Modal Ethereum Client
-export const ethereumClient = new EthereumClient(wagmiClient, chains);
-
-
-
-
 function MyApp({ Component, pageProps }: AppProps) {
   const AppComponent = Component as any;
   return (
-    <>
-    {/* <WagmiConfig client={wagmiClient}> */}
     <InjectTailwind>
     <AppComponent {...pageProps} />
     </InjectTailwind>
-    {/* </WagmiConfig> */}
-
-
-{/* <Web3Modal
-projectId={ProjectId}
-ethereumClient={ethereumClient}
-/> */}
-</>
   )
 }
 
