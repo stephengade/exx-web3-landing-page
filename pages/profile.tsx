a8eb77ab3eb649413b43dbb8b8b22c752d66fe44
Stephen Gbolagade <61961222+stephengade@users.noreply.github.com>
2022-12-21 12:28:23 +0100

    Update profile.tsx

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -12,7 +12,7 @@ const Profile = () => {
     <NavBar />
        <section className="pt-[120px] md:pt-[150px] px-12 -z-[50] bg-white">
      <h2 className="font-gros text-sm md:text-2xl text-exxDark">Welcome!</h2>
-     <p className="text-base font-sans">Your wallet address is</p>
+     <p className="text-base font-sans">Your wallet address is {address}</p>
     </section>
     </>
   )
